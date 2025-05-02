e70f03f620c53b0b241c12959f60ce772ca6c9e0
pthiruna <innovation.prakash@gmail.com>
2025-05-01 20:43:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ echo "creating sym links"
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.config/ ~/.config
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
